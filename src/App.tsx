cb1ab648220a14eea307d988417ea2be3df51f12
masayadegudegu <deguramasa@gmail.com>
2025-07-08 12:38:05 +0900

    Fix ApiKeyModal import error

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+@@ .. @@
+ import { AuthModal } from './components/AuthModal';
+ import { UserMenu } from './components/UserMenu';
+ import { ProjectInputForm } from './components/ProjectInputForm';
+ import { ProjectFlowDisplay } from './components/ProjectFlowDisplay';
+ import { LoadingSpinner } from './components/LoadingSpinner';
+ import { ErrorMessage } from './components/ErrorMessage';
+ import { ProjectLibraryModal } from './components/ProjectLibraryModal';
+-import { ApiKeyModal } from './components/ApiKeyModal';
++import ApiKeyModal from './components/ApiKeyModal';
+ import { supabase } from './lib/supabase';
